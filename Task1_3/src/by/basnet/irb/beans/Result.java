47236416449baaabe6e4c4e9cb8027a1b2f75711
Aliaksandr Dvornik <aadvornik@gmail.com>
2021-03-13 01:35:36 +0300

    Added new class to beans

---
+++ Task1_3/src/by/basnet/irb/beans/Result.java
@@ -0,0 +1,44 @@
+package by.basnet.irb.beans;
+
+import java.util.Date;
+
+public class Result {
+    String login;
+    String name;
+    Date date;
+    int mark;
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public int getMark() {
+        return mark;
+    }
+
+    public void setMark(int mark) {
+        this.mark = mark;
+    }
+
+
+}
