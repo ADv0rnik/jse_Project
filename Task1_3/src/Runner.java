e8a0495ca037d99c8f4c9c06036137621a131ed7
Aliaksandr Dvornik <aadvornik@gmail.com>
2021-03-16 13:31:57 +0300

    Updated

--- Task1_3/src/Runner.java
+++ Task1_3/src/Runner.java
@@ -1,9 +1,16 @@
+import by.basnet.irb.beans.DecimalMark;
+import by.basnet.irb.beans.HalfResult;
+import by.basnet.irb.beans.Mark;
 import by.basnet.irb.beans.Result;
 
 public class Runner {
     public static void main(String[] args) {
-        Result result = new Result("cool", "xml", "2013-03-27","8");
+        Mark mark1 = new HalfResult(20);
+        Mark mark = new DecimalMark("9.5");
+        Result result = new Result("cool", "xml", "2013-03-27", mark1);
         System.out.println(result.toString());
+        /*Result result1 = new HalfResult("cool", "xml", "2013-03-27","11.0");
+        System.out.println(result1.toString());*/
 
     }
 }
