a2929ed1c07b88f57dee73a05fb8f049c1003206
Aliaksandr Dvornik <aadvornik@gmail.com>
2021-03-17 00:35:52 +0300

    New class for loading data from source

---
+++ Task1_3/src/by/basnet/irb/ResultLoader.java
@@ -0,0 +1,14 @@
+package by.basnet.irb;
+
+import by.basnet.irb.dao.IResultDAO;
+
+import java.sql.Connection;
+import java.sql.Statement;
+
+public class ResultLoader {
+    public static void loadResults(IResultDAO reader){
+        Connection conn = null;
+        Statement stmt = null;
+
+    }
+}
