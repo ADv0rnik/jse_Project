739bc17a79884a8db3433689a81162bb3d95d250
Aliaksandr Dvornik <aadvornik@gmail.com>
2021-03-17 00:36:24 +0300

    Runner for checking beans

--- Task1_3/src/Runner.java
+++ Task1_3/src/Runner.java
@@ -5,12 +5,12 @@ import by.basnet.irb.beans.Result;
 
 public class Runner {
     public static void main(String[] args) {
-        Mark mark1 = new HalfResult(20);
+        Mark mark1 = new HalfResult("20.5");
         Mark mark = new DecimalMark("9.5");
+        Mark mark2 = new Mark("8");
         Result result = new Result("cool", "xml", "2013-03-27", mark1);
         System.out.println(result.toString());
-        /*Result result1 = new HalfResult("cool", "xml", "2013-03-27","11.0");
-        System.out.println(result1.toString());*/
+
 
     }
 }
