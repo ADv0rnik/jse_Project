67ec741fb80bb47110a8c7cf1ae2cd87fd178618
Aliaksandr Dvornik <aadvornik@gmail.com>
2021-03-17 01:14:12 +0300

    Added constant statements

---
+++ Task1_3/src/by/basnet/irb/statements/Statements.java
@@ -0,0 +1,8 @@
+package by.basnet.irb.statements;
+
+public class Statements {
+    public static final String DB_URL = "jdbc:mysql://127.0.0.1:3306/mysql";
+    public static final String USER = "root";
+    public static final String PASS = "root";
+
+}
