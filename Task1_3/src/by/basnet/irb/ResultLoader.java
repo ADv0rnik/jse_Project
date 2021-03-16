2d0b8098fda7625939a888c4b76ab71dee95d061
Aliaksandr Dvornik <aadvornik@gmail.com>
2021-03-17 01:14:39 +0300

    Updated

--- Task1_3/src/by/basnet/irb/ResultLoader.java
+++ Task1_3/src/by/basnet/irb/ResultLoader.java
@@ -1,14 +1,20 @@
 package by.basnet.irb;
 
 import by.basnet.irb.dao.IResultDAO;
+import by.basnet.irb.database.connection.DBConnector;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.Statement;
+
 
 public class ResultLoader {
     public static void loadResults(IResultDAO reader){
         Connection conn = null;
         Statement stmt = null;
+        PreparedStatement preparedStatement = null;
+        conn = DBConnector.getConnection();
+
 
     }
 }
