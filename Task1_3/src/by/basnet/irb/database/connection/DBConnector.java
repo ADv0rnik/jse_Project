48b6d25d6fc66a61e4f06c8e6b2e7377ac9a799b
Aliaksandr Dvornik <aadvornik@gmail.com>
2021-03-18 16:37:48 +0300

    Updated methods

--- Task1_3/src/by/basnet/irb/database/connection/DBConnector.java
+++ Task1_3/src/by/basnet/irb/database/connection/DBConnector.java
@@ -1,27 +1,65 @@
 package by.basnet.irb.database.connection;
 
+import by.basnet.irb.beans.Result;
 import exceptions.DBExceptions;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
+
 import static by.basnet.irb.database.managment.Statements.*;
 
 public final class DBConnector {
-    static Connection connection = null;
+    private static Connection connection = null;
+    private static Statement statement = null;
+
 
     public DBConnector() {
     }
 
     public static Connection getConnection(){
         try{
-            connection = DriverManager.getConnection(DB_URL, USER, PASS);
-
+            if(connection == null){
+                connection = DriverManager.getConnection(DB_URL, USER, PASS);
+            }
         } catch (SQLException e) {
             throw new DBExceptions("Database connecting error: " + DB_URL, e);
         }
         return connection;
     }
 
+    public static void connectionClose(Connection c){
+        try{
+            if(c!=null){
+                c.close();
+            }
+        } catch (SQLException e){
+            throw new DBExceptions("Connection error: "+ Connection.class.getSimpleName(), e);
+        }
+    }
+
+    public static int getUpdate (String s){
+            try {
+                if(statement == null) {
+                    statement = getConnection().createStatement();
+                }
+                return statement.executeUpdate(s);
+            } catch (SQLException e) {
+                throw new DBExceptions("Statement update error: ", e);
+            }
+    }
+
+    public static ResultSet getQuery(String s){
+        try{
+            if(statement == null){
+                statement = getConnection().createStatement();
+            }
+            return statement.executeQuery(s);
+        } catch (SQLException e){
+            throw new DBExceptions("Wrong SQL query: "+ s, e);
+        }
+    }
+
 
 }
+
+
+
