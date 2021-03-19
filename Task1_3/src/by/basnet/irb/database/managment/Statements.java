6edfde2d6b179ccd268a7ab024e7259054e37e00
Aliaksandr Dvornik <aadvornik@gmail.com>
2021-03-19 19:24:51 +0300

    Updated

--- Task1_3/src/by/basnet/irb/database/managment/Statements.java
+++ Task1_3/src/by/basnet/irb/database/managment/Statements.java
@@ -5,4 +5,9 @@ public class Statements {
     public static final String USER = "root";
     public static final String PASS = "root";
 
+    public static final String CLEAR_LOGINS = "TRUNCATE TABLE logins;";
+    public static final String CLEAR_TESTS = "TRUNCATE TABLE tests;";
+    public static final String CLEAR_RESULTS = "TRUNCATE TABLE results;";
+
+
 }
